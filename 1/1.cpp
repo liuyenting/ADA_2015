2352d333b171f1b8fc680b814f418214df34ee16
Andy <Andy@localhost.local>
2015-09-17 09:10:12 +0800

    p1: initialized

---
+++ 1/1.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+
+int main()
+{
+    int cases;
+    std::cin >> cases;
+    
+    int a, b;
+    for(int i = 0; i < cases; i++)
+    {
+        std::cin >> a >> b;
+        std::cout << (a+b) << std::endl;
+    }
+
+    return 0;
+}
