18687ca993a2ccdc30a8c880ef6b73ba46d67b16
Liu Yen-Ting <windows.linux.mac@gmail.com>
2015-10-11 14:45:04 +0800

    p3: trying to enlarge the data type

--- 3/queue.cpp
+++ 3/queue.cpp
@@ -6,9 +6,9 @@
 
 #define MAX_N 200000
 
-unsigned long long pow_n_mod(unsigned int base, unsigned long long exponent, unsigned long modulus)
+long long pow_n_mod(long long base, long long exponent, long long modulus)
 {
-	unsigned long long product = 1;
+	long long product = 1;
 
 	while (exponent > 0)
 	{
@@ -27,112 +27,37 @@ unsigned long long pow_n_mod(unsigned int base, unsigned long long exponent, uns
 	return product;
 }
 
+long long wrapped_mod(long long i, long long i_max)
+{
+	i %= i_max;
+	return (i < 0) ? i+i_max : i;
+}
+
 struct queue_comparer
 {
-	unsigned int n;              // number of people
-	unsigned long c, p;          // c:constant, p:modulus, p2:criterion
-	unsigned long long e;        // e:exponent
+	long long n;             // number of people
+	long long c, p;          // c:constant, p:modulus, p2:criterion
+	long long e;          // e:exponent
 
-	bool operator()(const unsigned long long& i, const unsigned long long& j)
+	bool operator()(const long long& i, const long long& j)
 	{
 		// ab mod n = ((a mod n)(b mod n)) mod n;
-		long long var = ( ((c*(i-j)) % p) * (pow_n_mod(i+j, e, p)) ) % p;
+		long long first = wrapped_mod(c*(i-j), p);
+		long long second = pow_n_mod(i+j, e, p);
+		long long var = (first * second) % p;
 
-		std::cerr << "i = " << i << ", j = " << j << std::endl;
-		std::cerr << "-> " << c << "*(" << i << "-" << j << ")%" << p << " = " << (c*(i-j)) % p << std::endl;
-		std::cerr << "-> ( (" << i << "+" << j << ")^" << e << " )%" << p << " = " << pow_n_mod(i+j, e, p) << std::endl;
-		std::cerr << "-> (a*b)%" << p << " = " << var << std::endl << std::endl;
-
+		/*
+		   std::cerr << "i = " << i << ", j = " << j << std::endl;
+		   std::cerr << "-> " << c << "*(" << i << "-" << j << ")%" << p << " = " << (c*(i-j)) % p << std::endl;
+		   std::cerr << "-> ( (" << i << "+" << j << ")^" << e << " )%" << p << " = " << pow_n_mod(i+j, e, p) << std::endl;
+		   std::cerr << "-> (a*b)%" << p << " = " << var << std::endl << std::endl;
+		 */
+		std::cerr << "first = " << first << ", second = " << second << std::endl;
+		std::cerr << "var = " << var << ", p/2 = " << (float)p/2 << ", var>p/2 = " << (2*var>p) << std::endl;
 		// var > 2*p -> 2*var > p
 		return 2 * var > p;
 	}
 };
-// setup the comparer.
-queue_comparer comparer;
-
-std::vector<unsigned int> merge(std::vector<unsigned int> &vec,
-                                const std::vector<unsigned int>& left,
-                                const std::vector<unsigned int>& right)
-{
-	// Fill the resultant vector with sorted results from both vectors
-	std::vector<unsigned int> result;
-	unsigned left_it = 0, right_it = 0;
-
-	while (left_it < left.size() && right_it < right.size())
-	{
-		// If the left value is smaller than the right it goes next
-		// into the resultant vector
-		/*
-		   if (left[left_it] < right[right_it])
-		   {
-		   result.push_back(left[left_it]);
-		   left_it++;
-		   }
-		   else
-		   {
-		   result.push_back(right[right_it]);
-		   right_it++;
-		   }
-		 */
-
-		if (comparer(left[left_it], right[right_it]))
-		{
-			result.push_back(right[right_it]);
-			right_it++;
-		}
-		else
-		{
-			result.push_back(left[left_it]);
-			left_it++;
-		}
-	}
-
-	// Push the remaining data from both vectors onto the resultant
-	while (left_it < left.size())
-	{
-		result.push_back(left[left_it]);
-		left_it++;
-	}
-
-	while (right_it < right.size())
-	{
-		result.push_back(right[right_it]);
-		right_it++;
-	}
-	// show merge process
-	int i;
-	for (i=0; i<result.size(); i++)
-	{
-		std::cout << result[i] << " ";
-	}
-	// break each line for display purposes
-	std::cout << "***********" << std::endl;
-
-	// take a source vector and parse the result to it. then return it.
-	vec = result;
-	return vec;
-}
-
-std::vector<unsigned int> merge_sort(std::vector<unsigned int>& vec)
-{
-	// terminate when list only contains a single element
-	if (vec.size() == 1)
-		return vec;
-
-	// determine the location of the middle element in the vector
-	std::vector<unsigned int>::iterator middle = vec.begin() + (vec.size() / 2);
-
-	std::vector<unsigned int> left(vec.begin(), middle);
-	std::vector<unsigned int> right(middle, vec.end());
-
-	// perform a merge sort on the two smaller vectors
-	left = merge_sort(left);
-	right = merge_sort(right);
-
-	return merge(vec,left, right);
-}
-
-
 
 int main(void)
 {
@@ -140,10 +65,11 @@ int main(void)
 	std::cin >> cases;
 
 	// pre-allocate the maximum possible size of the queue.
-	std::vector<unsigned int> queue;
+	std::vector<long long> queue;
 	queue.reserve(MAX_N);
 
-
+	// setup the comparer.
+	queue_comparer comparer;
 
 	while (cases-- > 0)
 	{
@@ -156,8 +82,21 @@ int main(void)
 		queue.resize(comparer.n);
 		std::iota(std::begin(queue), std::end(queue), 1);
 
-		std::sort(queue.begin(), queue.end(), comparer);
-		//merge_sort(queue);
+		//std::sort(queue.begin(), queue.end(), comparer);
+		bool sortDone = 0;
+		while (sortDone == 0)
+		{
+			sortDone = 1;
+			for (int i = 0; i < queue.size() - 1; i++)
+			{
+				if (comparer(queue[i + 1], queue[i])) {
+					long long j = queue[i + 1];
+					queue[i + 1] = queue[i];
+					queue[i] = j;
+					sortDone = 0;
+				}
+			}
+		}
 
 		// print out the result
 		for (auto i = queue.begin(); i != queue.end(); ++i)
