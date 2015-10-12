24c4738d72bbdca4dd6af6e5ae4447787aebeb11
Liu Yen-Ting <windows.linux.mac@gmail.com>
2015-10-12 10:26:09 +0800

    p3: try to use array instead of vector

--- 3/3.cpp
+++ 3/3.cpp
@@ -7,9 +7,11 @@
 
 #define MAX_N 200000
 
-static int n, n2;                 // number of people
+static int n;                     // number of people
 static long long c, p;            // c:constant, p:modulus
 static long long e;               // e:exponent
+
+static int ids[MAX_N];            // the queue
 static long long lut[2 * MAX_N];  // lookup table for pow_n_mod
 
 long long pow_n_mod(long long base,
@@ -42,12 +44,10 @@ inline long long wrapped_mod(long long i, const long long& i_max)
 bool comparer(const long long& i, const long long& j)
 {
 	if (lut[i+j-1] == -1)
-	{
-		// ab mod n = ((a mod n)(b mod n)) mod n;
-		// var > 2*p -> 2*var > p
 		lut[i+j-1] = pow_n_mod(i+j, e, p);
-	}
 
+	// ab mod n = ((a mod n)(b mod n)) mod n;
+	// var > 2*p -> 2*var > p
 	return (2 * ((wrapped_mod(c*(i-j), p) * lut[i+j-1]) % p) > p);
 }
 
@@ -57,30 +57,33 @@ int main(void)
 	std::cin >> cases;
 
 	// pre-allocate the maximum possible size of the queue.
-	std::vector<int> queue;
-	queue.reserve(MAX_N);
+	//std::vector<int> queue;
+	//queue.reserve(MAX_N);
 
 	while (cases-- > 0)
 	{
 		// read the values and prep them.
 		std::cin >> n >> c >> e >> p;
-		n2 = n * 2;
 
 		// get the iterator
-		auto end_it = queue.begin();
-		std::advance(end_it, n);
+		//auto end_it = queue.begin();
+		//std::advance(end_it, n);
 
 		// reset the lut
 		std::memset(lut, -1, sizeof(long long) * 2 * n);
 
 		// fill with ids
-		std::iota(queue.begin(), end_it, 1);
+		//std::iota(queue.begin(), end_it, 1);
+		std::iota(ids, ids + n, 1);
 
-		std::sort(queue.begin(), end_it, comparer);
+		//std::sort(queue.begin(), end_it, comparer);
+		std::sort(ids, ids + n, comparer);
 
 		// print out the result
-		for (auto i = queue.begin(); i != end_it; ++i)
-			std::cout << *i << ' ';
+		//for (auto i = queue.begin(); i != end_it; ++i)
+		//	std::cout << *i << ' ';
+		for (int i = 0; i < n; i++)
+			std::cout << ids[i] << ' ';
 		std::cout << std::endl;
 	}
 
