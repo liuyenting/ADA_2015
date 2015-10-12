e76ac23a4d8f6dce84fb0bc00dbbf17781e9a09c
Liu Yen-Ting <windows.linux.mac@gmail.com>
2015-10-12 10:46:47 +0800

    p3: using merge sort

--- 3/3.cpp
+++ 3/3.cpp
@@ -4,6 +4,7 @@
 #include <iterator>  // std::begin, std::end
 #include <numeric>   // std::iota
 #include <cstring>
+#include <cstdlib>
 
 #define MAX_N 200000
 
@@ -35,14 +36,11 @@ long long pow_n_mod(long long base,
 	return product;
 }
 
-/*
-   inline long long wrapped_mod(long long i, const long long& i_max)
-   {
-   return ;
-   }
- */
+inline long long wrapped_mod(long long i, const long long& i_max)
+{
+	return ((i % i_max + i_max) % i_max);
+}
 
-#define WRAPPED_MOD(i, i_max) ((i % i_max + i_max) % i_max)
 bool comparer(const long long& i, const long long& j)
 {
 	if (lut[i+j-1] == -1)
@@ -50,7 +48,34 @@ bool comparer(const long long& i, const long long& j)
 
 	// ab mod n = ((a mod n)(b mod n)) mod n;
 	// var > 2*p -> 2*var > p
-	return (2 * ((WRAPPED_MOD(c*(i-j), p) * lut[i+j-1]) % p) > p);
+	return (2 * ((wrapped_mod(c*(i-j), p) * lut[i+j-1]) % p) > p);
+}
+
+void merge (int *a, int n, int m) {
+	int i, j, k;
+	int *x = (int*)malloc(n * sizeof (int));
+	for (i = 0, j = m, k = 0; k < n; k++)
+	{
+		x[k] = (j == n) ? a[i++]
+									: (i == m) ? a[j++]
+		       //: a[j] < a[i] ? a[j++]
+									: (comparer(a[j], a[i])) ? a[j++]
+									: a[i++];
+	}
+	for (i = 0; i < n; i++)
+	{
+		a[i] = x[i];
+	}
+	free(x);
+}
+
+void merge_sort (int *a, int n) {
+	if (n < 2)
+		return;
+	int m = n / 2;
+	merge_sort(a, m);
+	merge_sort(a + m, n - m);
+	merge(a, n, m);
 }
 
 int main(void)
@@ -79,7 +104,8 @@ int main(void)
 		std::iota(ids, ids + n, 1);
 
 		//std::sort(queue.begin(), end_it, comparer);
-		std::sort(ids, ids + n, comparer);
+		//std::sort(ids, ids + n, comparer);
+		merge_sort(ids, n);
 
 		// print out the result
 		//for (auto i = queue.begin(); i != end_it; ++i)
