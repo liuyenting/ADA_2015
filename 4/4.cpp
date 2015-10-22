d0ce6fe8ea23dae170bce0f61744a2f001203e77
Liu Yen-Ting <windows.linux.mac@gmail.com>
2015-10-22 23:06:14 +0800

    p4: bump by 7

--- 4/4.cpp
+++ 4/4.cpp
@@ -26,7 +26,7 @@
  *                                                               x
  */
 
-#define M 0x71c71c71c71c71c7
+static const uint64_t M = 0x71c71c71c71c71c7;
 static const uint64_t Q[7] = {30, 15, 9, 3, 3, 3, 3};
 static const uint64_t R[7] = {0x3fffffff, 0x00007fff, 0x000001ff, 0x00000007, 0x00000007, 0x00000007, 0x00000007};
 
@@ -39,36 +39,35 @@ int main(void) {
 	uint64_t n;        // numerator
 	uint64_t m;        // n % d goes here.
 
-	int8_t parity;
-	uint8_t dummy;
+	uint8_t seven, four, dummy;
 	uint64_t c, t;
 
 	while(cases-- > 0) {
-		c = 0;
 		std::cin >> l >> r;
 
-		for(n = l; n <= r; n++) {
-			// mod 7
-			// 7 = (1 << 3) - 1
-			m = (n & M) + ((n >> 3) & M);
+		// check the first one...
+		// mod 7, 7 = (1 << 3) - 1
+		m = (l & M) + ((l >> 3) & M);
 
-			for(const uint64_t * q = &Q[0], * r = &R[0]; m > 7; q++, r++) {
-				m = (m >> *q) + (m & *r);
+		for(const uint64_t *q = &Q[0], *r = &R[0]; m > 7; q++, r++) {
+			m = (m >> *q) + (m & *r);
+		}
+		//m = (m == 7) ? 0 : m;
+		// less portably:
+		//m = m & -((signed)(m - d) >> s);
+
+		// wrap around
+		m = 7-m;
+
+		/// ...bump by 7
+		for(n = l+m, c = 0; n <= r; n += 7) {
+			for(t = n, seven = four = 0; t > 0; t /= 10) {
+				dummy = t % 10;
+				if(dummy == 7) seven++;
+				else if(dummy == 4) four++;
 			}
-			//m = m == 7 ? 0 : m;        // OR, less portably: m = m & -((signed)(m - d) >> s);
 
-			if(m == 7) {
-				parity = -2;
-				for(t = n; t > 0; t /= 10) {
-					dummy = t % 10;
-					if(dummy == 7)
-						parity++;
-					else if(dummy == 4)
-						parity--;
-				}
-
-				c += (parity > 0);
-			}
+			c += ((seven > four) & (seven >= 3));
 		}
 
 		std::cout << c << std::endl;
