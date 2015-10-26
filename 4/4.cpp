8c4b4682e782f32a3cf461b115683a5bde9ae573
Liu Yen-Ting <windows.linux.mac@gmail.com>
2015-10-26 23:48:42 +0800

    p4: recursion to iteration

--- 4/4.cpp
+++ 4/4.cpp
@@ -1,9 +1,9 @@
 #include <iostream>
-#include <cstdint>
-#include <x86intrin.h>
 #include <vector>
+#include <stack>
 
 struct prefix {
+	unsigned int level;
 	unsigned long long value;
 	unsigned int remainder;
 	unsigned int seven;
@@ -22,47 +22,103 @@ static const unsigned int remainder_lookup[7][10] = {
 
 static unsigned int lower_digits, upper_digits;
 static unsigned long long lower, upper;
-static unsigned int lower_result, upper_result;
-static bool toggled = false;
+static unsigned int counter, lower_result, upper_result;
 
-void count_lucky_numbers(unsigned int digits, struct prefix prefix) {
-	if(digits > upper_digits) {
-		if((prefix.remainder == 0) && (prefix.seven >= 3) && (prefix.seven > prefix.four)) {
-			//std::cerr << prefix.value << std::endl;
-
-			upper_result++;
-			if(prefix.value < lower)
-				lower_result++;
-		}
-
-		return;
-	}
-
-	for(unsigned int i = 0; i <= 9; i++) {
-		struct prefix new_prefix = {
-			.value = prefix.value*10+i,
-			.remainder = remainder_lookup[prefix.remainder][i],
-			.seven = prefix.seven+(i==7),
-			.four = prefix.four+(i==4)
-		};
-
-		count_lucky_numbers(digits+1, new_prefix);
-	}
-}
+/*
+ * void count_lucky_numbers(unsigned int digits, struct prefix prefix) {
+ * if(digits > upper_digits) {
+ * if((prefix.remainder == 0) && (prefix.seven >= 3) && (prefix.seven > prefix.four)) {
+ * //std::cerr << prefix.value << std::endl;
+ *
+ * upper_result++;
+ * if(prefix.value < lower)
+ *  lower_result++;
+ * }
+ *
+ * return;
+ * }
+ *
+ * for(unsigned int i = 0; i <= 9; i++) {
+ * struct prefix new_prefix = {
+ * .value = prefix.value*10+i,
+ * .remainder = remainder_lookup[prefix.remainder][i],
+ * .seven = prefix.seven+(i==7),
+ * .four = prefix.four+(i==4)
+ * };
+ *
+ * count_lucky_numbers(digits+1, new_prefix);
+ * }
+ * }
+ */
 
 void count_lucky_numbers() {
-	lower_result = upper_result = 0;
+	counter = lower_result = upper_result = 0;
+
+	struct prefix old_prefix, new_prefix;
+	std::vector<struct prefix> number;
+	number.reserve(1024);
+
+	// push the first element.
 	for(unsigned int i = 0; i <= 9; i++) {
-		struct prefix prefix = {
+		new_prefix = {
+			.level = 1,
 			.value = i,
 			.remainder = i%7,
 			.seven = (i==7),
 			.four = (i==4)
 		};
-
-		count_lucky_numbers(1, prefix);
+		number.push_back(new_prefix);
 	}
+
+	unsigned long long depth = 0;
+
+	while(!number.empty()) {
+		//if(number.size() > depth)
+		//	depth = number.size();
+
+		old_prefix = number.back();
+		number.pop_back();
+
+		if(old_prefix.level > upper_digits) {
+			if((old_prefix.remainder == 0) && (old_prefix.seven >= 3) && (old_prefix.seven > old_prefix.four)) {
+				//std::cerr << old_prefix.value << std::endl;
+				if(old_prefix.value < upper)
+					upper_result++;
+				if(old_prefix.value < lower)
+					lower_result++;
+			}
+		} else {
+			for(unsigned int i = 0; i <= 9; i++) {
+				new_prefix = {
+					.level = old_prefix.level + 1,
+					.value = old_prefix.value*10 + i,
+					.remainder = remainder_lookup[old_prefix.remainder][i],
+					.seven = old_prefix.seven + (i==7),
+					.four = old_prefix.four + (i==4)
+				};
+				number.push_back(new_prefix);
+			}
+		}
+	}
+
+	//std::cerr << "maximum depth = " << depth << std::endl;
 }
+
+/*
+ * void count_lucky_numbers() {
+ * lower_result = upper_result = 0;
+ * for(unsigned int i = 0; i <= 9; i++) {
+ * struct prefix prefix = {
+ * .value = i,
+ * .remainder = i%7,
+ * .seven = (i==7),
+ * .four = (i==4)
+ * };
+ *
+ * count_lucky_numbers(1, prefix);
+ * }
+ * }
+ */
 
 int main(void) {
 	// disable buffering
@@ -86,13 +142,11 @@ int main(void) {
 		}
 		upper_digits--;
 
-		toggled = false;
-
 		count_lucky_numbers();
 
-		std::cerr << "upper result = " << upper_result << std::endl;
-		std::cerr << "lower result = " << lower_result << std::endl;
-		std::cerr << "[" << lower << "," << upper << "] = ";
+		//std::cerr << "upper result = " << upper_result << std::endl;
+		//std::cerr << "lower result = " << lower_result << std::endl;
+		//std::cerr << "[" << lower << "," << upper << "] = ";
 		std::cout << (upper_result-lower_result) << std::endl;
 
 		//std::cerr << "l digits = " << min_digits << std::endl;
