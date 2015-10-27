1a3380c4e160aeacdc6fe82bcf30c15d5a8ab793
Liu Yen-Ting <windows.linux.mac@gmail.com>
2015-10-27 23:46:07 +0800

    p4: DP complete implemented

--- 4/4.cpp
+++ 4/4.cpp
@@ -1,11 +1,10 @@
 #include <iostream>
-#include <vector>
 
-struct prefix {
-	unsigned int level;
-	unsigned long long value;
-	unsigned int seven;
-	unsigned int four;
+#define MAX_DIGITS 18
+
+/* 10^n % 7 */
+static const unsigned int exponent_ratio_lookup[19] = {
+	1, 3, 2, 6, 4, 5, 1, 3, 2, 6, 4, 5, 1, 3, 2, 6, 4, 5, 1
 };
 
 static const unsigned long long ratio[20] = {
@@ -31,76 +30,70 @@ static const unsigned long long ratio[20] = {
 	1000000000000000000
 };
 
-static unsigned int upper_digits;
-static unsigned long long lower, upper;
-static unsigned int lower_result, upper_result;
-
-static struct prefix number[1024];
-static unsigned int DP[18][10];
+/*
+ * 1) current digits (starts from 1)
+ * 2) mod 7
+ * 3) amount of digit 7
+ * 4) amount of digit 4
+ */
+static unsigned int table[MAX_DIGITS+1][7][MAX_DIGITS+1][MAX_DIGITS+1];
 
 int main(void) {
-	// disable buffering
-	std::ios_base::sync_with_stdio(false);
+	// write the default conditions.
+	table[0][0][0][0] = 1;
 
-	unsigned int cases;
-	std::cin >> cases;
+	// filling rest of the digits.
+	unsigned int digit_pos, mod, seven_cnt, four_cnt, digit;
+	for(digit_pos = 1; digit_pos <= MAX_DIGITS; digit_pos++) {
+		for(mod = 0; mod < 7; mod++) {
+			for(seven_cnt = 0; seven_cnt <= digit_pos; seven_cnt++) {
+				for(four_cnt = 0; four_cnt <= digit_pos; four_cnt++) {
+					for(digit = 0; digit <= 9; digit++) {
+						unsigned int new_mod = ((digit % 7) * exponent_ratio_lookup[digit_pos]) % 7;
+						//unsigned long long new_mod = (digit * ratio[digit_pos]) % 7;
 
-	while(cases-- > 0) {
-		std::cin >> lower >> upper;
+						if(mod < new_mod)
+							new_mod = 7 + mod - new_mod;
+						else
+							new_mod = mod - new_mod;
 
-		// test for digits.
-		for(unsigned int i = 1; i < 19; i++) {
-			if(ratio[i] >= upper) {
-				upper_digits = i;
-				break;
+						unsigned int pulled_value = 0;
+						switch(digit) {
+						case 4:
+							pulled_value = (four_cnt) ? (table[digit_pos - 1][new_mod][seven_cnt][four_cnt - 1]) : 0;
+							break;
+						case 7:
+							pulled_value = (seven_cnt) ? (table[digit_pos - 1][new_mod][seven_cnt - 1][four_cnt]) : 0;
+							break;
+						default:
+							pulled_value = table[digit_pos - 1][new_mod][seven_cnt][four_cnt];
+						}
+
+						table[digit_pos][mod][seven_cnt][four_cnt] += pulled_value;
+					}
+				}
 			}
 		}
-		upper_digits--; // no need for full set of digits
-
-		lower_result = upper_result = 0;
-
-		int index = 0;
-
-		// push the first element.
-		for(unsigned int i = 0; i <= 9; i++) {
-			number[index++] = {
-				.level = 1,
-				.value = i,
-				.seven = (i==7),
-				.four = (i==4)
-			};
-		}
-
-		while(index-- > 0) {
-			if(number[index].level == upper_digits) {
-				if((number[index].seven >= 3) && (number[index].seven > number[index].four) && (number[index].value % 7 == 0)) {
-					//std::cerr << number[index].value << std::endl;
-					upper_result += (number[index].value < upper);
-					lower_result += (number[index].value < lower);
-				}
-			} else {
-				// new item, i = 0
-				number[index++].level += 1;
-
-				// new items, i = 1~9
-				for(unsigned int i = 1; i <= 9; i++, index++) {
-					number[index].level = number[index-1].level;
-					number[index].value = number[index-1].value + ratio[number[index].level];
-					number[index].seven = number[index-1].seven;
-					number[index].four = number[index-1].four;
-				}
-
-				// add back 4 and 7 counter.
-				number[index-6].four++;
-				number[index-3].seven++;
-			}
-		}
-
-		//std::cerr << "upper result = " << upper_result << std::endl;
-		//std::cerr << "lower result = " << lower_result << std::endl;
-		//std::cerr << "[" << lower << "," << upper << "] = ";
-		std::cout << (upper_result-lower_result) << std::endl;
 	}
+
+	// start the lookup.
+	unsigned long long lower_bound, upper_bound;
+
+	lower_bound = 800, upper_bound = 10000;
+
+	// DEBUG test
+	unsigned int result = 0;
+	digit_pos = 18;
+	for(seven_cnt = 3; seven_cnt <= digit_pos; seven_cnt++) {
+		for(four_cnt = 0; four_cnt < seven_cnt; four_cnt++) {
+			unsigned int pulled = table[digit_pos][0][seven_cnt][four_cnt];
+			//if(pulled > 0)
+			//	std::cerr << "digit_pos = " << digit_pos << ", seven_cnt = " << seven_cnt << ", four_cnt = " << four_cnt << ", pulled = " << pulled << std::endl;
+
+			result += pulled;
+		}
+	}
+	std::cout << "result = " << result << std::endl;
 
 	return 0;
 }
