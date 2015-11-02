316e2b29934587b9be8d802d37d823e6cc6388d8
Liu Yen-Ting <windows.linux.mac@gmail.com>
2015-11-02 16:44:18 +0800

    p5: moving naive implementation to array

--- 5/5.cpp
+++ 5/5.cpp
@@ -1,5 +1,7 @@
 #include <iostream>
 
+#define MAX_DIGIT 16
+
 int checksum(int num) {
 	int chksum = 0;
 	while(num > 0) {
@@ -9,12 +11,65 @@ int checksum(int num) {
 	return chksum;
 }
 
+int increase_by_one(int (&digits)[MAX_DIGIT]) {
+	digits[0]++;
+
+	int chksum = 0, carry = 0;
+	// carry out the numbers.
+	for(int j = 0; j < MAX_DIGIT; j++) {
+		digits[j] += carry;
+		carry = digits[j] / 10;
+		digits[j] %= 10;
+
+		chksum += digits[j];
+	}
+	return chksum;
+}
+
+void print_digits(int (&digits)[MAX_DIGIT]) {
+	bool leading_zero = true;
+	for(int j = MAX_DIGIT-1; j >= 0; j--) {
+		// don't print the leading zeros.
+		if(digits[j] == 0) {
+			if(leading_zero)
+				continue;
+		} else {
+			if(leading_zero)
+				leading_zero = false;
+		}
+
+		std::cout << digits[j];
+	}
+}
+
 int main(void) {
+	/*
+	   int test = 123, chksum, tmp;
+
+	   chksum = checksum(test);
+	   std::cout << "checksum of " << test << " is " << chksum << ".\n";
+
+	   int digits[MAX_DIGIT] = {0};
+	   tmp = test;
+	   for(int i = 0; i < MAX_DIGIT; i++) {
+	   digits[i] = tmp%10;
+	   tmp /= 10;
+	   }
+
+	   chksum = increase_by_one(digits);
+	   std::cout << "checksum of increase_by_one(" << test << ") = ";
+	   print_digits(digits);
+	   std::cout << " is " << chksum << ".\n";
+
+	   return 0;
+	 */
+
 	int cases;
 	std::cin >> cases;
 
 	int days;
 	int *checksums;
+	int checksum;
 	while(cases-- > 0) {
 		std::cin >> days;
 
@@ -23,16 +78,25 @@ int main(void) {
 		for(int i = 0; i < days; i++)
 			std::cin >> checksums[i];
 
-		int incomings = 0;
+		int digits[MAX_DIGIT] = {0};
+
+		// int incomings = 0;
 		for(int i = 0; i < days; i++) {
+			// calculate current checksum.
+			checksum = 0;
+			for(int j = 0; j < MAX_DIGIT; j++)
+				checksum += digits[j];
+
 			// force the increment, since s is strictly increasing.
-			if(checksum(incomings) == checksums[i])
-				incomings++;
+			if(checksum == checksums[i])
+				checksum = increase_by_one(digits);
 
 			// search for next valid value.
-			while(checksum(incomings) != checksums[i])
-				incomings++;
-			std::cout << incomings;
+			while(checksum != checksums[i])
+				checksum = increase_by_one(digits);
+			print_digits(digits);
+
+			// decide whether we should print the white space.
 			if(i != days-1)
 				std::cout << ' ';
 			else
