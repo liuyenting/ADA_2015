5ec096bb46bce3b299150295cd57931b55150359
Liu Yen-Ting <windows.linux.mac@gmail.com>
2015-11-08 18:20:21 +0800

    p5: rewrite

--- 5/5.cpp
+++ 5/5.cpp
@@ -1,236 +1,117 @@
 #include <iostream>
+#include <vector>
 
-#define MAX_DIGIT 16
-
-int checksum(int num) {
-	int chksum = 0;
-	while(num > 0) {
-		chksum += num % 10;
-		num /= 10;
-	}
-	return chksum;
-}
-
-void print_digits(int digit_cnt, int (&digits)[MAX_DIGIT]) {
-	// std::cout << "print_digits() called, max_digit = " << max_digit << std::endl;
-	bool leading_zero = true;
-	for(int j = digit_cnt-1; j >= 0; j--) {
-		// don't print the leading zeros.
-		if(digits[j] == 0) {
-			if(leading_zero)
-				continue;
-		} else {
-			if(leading_zero)
-				leading_zero = false;
-		}
-
-		std::cout << digits[j];
-	}
-}
+#define MAX_DIGITS 1000
 
 int main(void) {
-	#ifdef DEBUG
-	{
-		int test = 123, chksum, tmp, max_digit;
-
-		chksum = 0;
-		tmp = test;
-		while(tmp > 0) {
-			chksum += tmp % 10;
-			tmp /= 10;
-		}
-		std::cout << "checksum of " << test << " is " << chksum << ".\n";
-
-		int digits[MAX_DIGIT] = {0};
-		tmp = test;
-		for(int i = 0; i < MAX_DIGIT; i++) {
-			digits[i] = tmp%10;
-			tmp /= 10;
-		}
-
-		chksum = increase_by_one(&max_digit, digits);
-		std::cout << "checksum of increase_by_one(" << test << ") = ";
-		print_digits(digits);
-		std::cout << " is " << chksum << ".\n";
-
-		return 0;
-	}
-	#endif
-
-	int cases;
-	std::cin >> cases;
-
 	int days;
-	int* checksums;
-	while(cases-- > 0) {
-		std::cin >> days;
+	std::cin >> days;
 
-		// read the checksums.
-		checksums = (int*)malloc(sizeof(int) * days);
-		for(int i = 0; i < days; i++)
-			std::cin >> checksums[i];
+	std::vector<int> chksum(days);
+	for(int day = 0; day < days; day++)
+		std::cin >> chksum[day];
 
-		int digits[MAX_DIGIT] = {0}, digit_cnt;
-		int prev_chksum, curr_chksum, carry;
+	// Maximum of 1000 digits.
+	std::vector<int> digits(MAX_DIGITS);
+	int digit_pos = 0;
 
-		// complete the first case.
-		curr_chksum = checksums[0];
-		digit_cnt = curr_chksum/9 + 1;
-		for(int i = 0; i < digit_cnt-1; i++)
-			digits[i] = 9;
-		digits[digit_cnt-1] = curr_chksum%9;
+	int prev_chksum, curr_chksum, delta_chksum;
+	for(int day = 0; day < days; day++) {
+		if(day == 0) {
+			// Set the first checksum directly on the first day.
+			digits[digit_pos++] = chksum[0];
+		} else {
+			curr_chksum = chksum[day];
+			if(curr_chksum > prev_chksum) {
+				// std::cerr << '>' << std::endl;
 
-		// print the first case.
-		print_digits(digit_cnt, digits);
-		std::cout << ((days != 1) ? ' ' : '\n');
+				// Calculate how many checksum are required.
+				delta_chksum = curr_chksum - prev_chksum;
 
-		// find rest of the cases.
-		for(int i = 1; i < days; i++) {
-			prev_chksum = curr_chksum;
-			curr_chksum = checksums[i];
+				// Fill the lower digits as 9.
+				int fill_pos;
+				for(fill_pos = 0; fill_pos < digit_pos && delta_chksum > 0; fill_pos++) {
+					delta_chksum -= 9 - digits[fill_pos];
+					digits[fill_pos] = 9;
+				}
 
-			int j, delta_chksum;
-			if(prev_chksum > curr_chksum) {
-				std::cout << '\n' << '>';
-
-				bool complete;
-				do
-				{
-					complete = true;
-
-					int bnd;
-					// find the first non-zero position.
-					for(j = 0; j < MAX_DIGIT; j++) {
-						if(digits[j] != 0) {
-							bnd = j+1;
-							break;
-						}
+				// Check whether the fillings are enough.
+				if(delta_chksum > 9) {
+					// This case only occurs when carry out is needed.
+					for(; fill_pos < MAX_DIGITS && delta_chksum > 0; fill_pos++) {
+						digits[fill_pos] = 9;
+						delta_chksum -= digits[fill_pos];
 					}
-					// get the position next to it.
-					++bnd;
+				}
+				if(delta_chksum > 0) {
+					digits[fill_pos++] = delta_chksum;
+				} else if(delta_chksum < 0) {
+					// Too many 0, decrease the most significant digit.
+					digits[fill_pos-1] += delta_chksum;
+				}
 
-					std::cout << "position boundary = " << bnd << ", digit is " << digits[bnd-1] << std::endl;
+				// Update the digit boundary.
+				if(fill_pos > digit_pos)
+					digit_pos = fill_pos;
+			} else if(curr_chksum <= prev_chksum) {
+				// std::cerr << "<=" << std::endl;
 
-					// check whether more digits need to introduce here.
-					digits[bnd-1]++;
-					prev_chksum++;
-					std::cout << "digits: ";
-					print_digits(digit_cnt, digits);
-					std::cout << ", chksum = " << prev_chksum << std::endl;
-					// reset the lower digits.
-					if(digits[bnd-1] > curr_chksum) {
-						digits[bnd++] = 1;
-						prev_chksum = 1;
-						for(j = 0; j < bnd-1; j++)
-							digits[j] = 0;
-					} else {
-						for(j = 0; j < bnd-1; j++) {
-							prev_chksum -= digits[j];
-							digits[j] = 0;
-						}
-					}
+				// Gradually carry out the digits until checksum is smaller.
+				for(int i = 0; i < MAX_DIGITS; i++) {
+					// Wipe current digit.
+					prev_chksum -= digits[i];
+					digits[i] = 0;
 
-					// update digit count.
-					if(bnd > digit_cnt)
-						digit_cnt = bnd;
-
-					std::cout << "digits: ";
-					print_digits(digit_cnt, digits);
-					std::cout << ", chksum = " << prev_chksum << std::endl;
-
-					delta_chksum = curr_chksum-prev_chksum;
-					for(j = 0; j < bnd; j++) {
-						digits[j] += delta_chksum;
-						if(digits[j] > 9) {
-							delta_chksum = digits[j] - 9;
+					// Carry to the next digit, and handle the overflows.
+					int carry = 1;
+					for(int j = i+1; j < digit_pos && carry > 0; j++) {
+						// Carry in and update the checksum.
+						if(digits[j] + carry > 9) {
+							prev_chksum += 9 - digits[j];
+							carry -= 9 - digits[j];
 							digits[j] = 9;
-						} else if(digits[j] < 0) {
-							digits[j] = 0;
-							complete = false;
-						} else
-							break;
+						} else {
+							prev_chksum += carry;
+							digits[j] += carry;
+							carry = 0;
+						}
 					}
-				} while(!complete);
-/*
-    // carry out.
-    digits[digit_cnt-1]++;
-    if(digits[digit_cnt-1] > 9) {
-     digits[digit_cnt] = 1;
-     digits[digit_cnt-1] = 0;
-     digit_cnt++;
-    }
-    digits[digit_cnt++] = 1;
-    for(int j = 0; j < digit_cnt-1; j++)
-     digits[j] = 0;
 
-
-
-    // remained delta checksum.
-    delta_chksum = curr_chksum-1;
-
-    for(j = 0; j < MAX_DIGIT; j++) {
-     digits[j] += delta_chksum;
-     if(digits[j] > 9) {
-      delta_chksum = digits[j] - 9;
-      digits[j] = 9;
-     } else
-      break;
-    }
- */
-			} else if(prev_chksum == curr_chksum) {
-				std::cout << '=';
-
-				int bnd;
-				// find the first non-zero position.
-				for(j = 0; j < MAX_DIGIT; j++) {
-					if(digits[j] != 0) {
-						bnd = j+1;
-						break;
+					// Increase the digit boundary if overflow.
+					if(carry > 0) {
+						prev_chksum += carry;
+						digits[digit_pos++] += carry;
 					}
-				}
-				// get the position next to it.
-				++bnd;
-				if(bnd > digit_cnt)
-					digit_cnt = bnd;
 
-				// std::cout << "position boundary = " << bnd << std::endl;
-
-				// reset the lower digits.
-				digits[bnd-1]++;
-				for(j = 0; j < bnd-1; j++)
-					digits[j] = 0;
-
-				delta_chksum = curr_chksum-digits[bnd-1];
-				for(j = 0; j < MAX_DIGIT; j++) {
-					digits[j] += delta_chksum;
-					if(digits[j] > 9) {
-						delta_chksum = digits[j] - 9;
-						digits[j] = 9;
-					} else
+					if(prev_chksum <= curr_chksum)
 						break;
 				}
-			} else {
-				std::cout << '<';
 
-				delta_chksum = curr_chksum-prev_chksum;
-
-				for(j = 0; j < MAX_DIGIT; j++) {
-					digits[j] += delta_chksum;
-					if(digits[j] > 9) {
-						delta_chksum = digits[j] - 9;
-						digits[j] = 9;
-					} else {
-						++j;
-						break;
+				// Fill rest of the needed checksum.
+				delta_chksum = curr_chksum - prev_chksum;
+				for(int i = 0; i < digit_pos && delta_chksum > 0; i++) {
+					digits[i] = 9;
+					delta_chksum -= 9;
+					if(delta_chksum < 0) {
+						digits[i] += delta_chksum;
+						delta_chksum = 0;
 					}
 				}
-				if(j > digit_cnt)
-					digit_cnt = j;
 			}
-
-			print_digits(digit_cnt, digits);
-			std::cout << ((i != days-1) ? ' ' : '\n');
 		}
+
+		// Update previous checksum.
+		prev_chksum = chksum[day];
+
+		// Print out the result.
+		for(int i = digit_pos - 1; i >= 0; i--)
+			std::cout << digits[i];
+
+		// Print out the delimiters.
+		if(days != days - 1)
+			std::cout << ' ';
+		else
+			std::cout << std::endl;
 	}
 
 	return 0;
